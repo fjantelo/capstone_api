3949d1687a3c1b6d52e8dba08c79520e870b204b
Fernando Antelo <fjantelo@gmail.com>
2022-08-30 15:07:46 -0400

    Changes user_playlists controller to render new user information as json when association is created

--- app/controllers/user_playlists_controller.rb
+++ app/controllers/user_playlists_controller.rb
@@ -2,9 +2,13 @@ class UserPlaylistsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    user_playlist = UserPlaylist.new(user_id: User.find_by(email: params[:email]).id, playlist_id: params[:playlist_id])
+    new_user = User.find_by(email: params[:email])
+    user_playlist = UserPlaylist.new(user_id: new_user.id, playlist_id: params[:playlist_id])
     if user_playlist.save
-      render json: user_playlist.as_json
+      output = []
+      output << user_playlist
+      output << { name: new_user.name, id: new_user.id, email: new_user.email }
+      render json: output.as_json
     else
       render json: { "error": user_playlist.errors.full_messages },
              status: :unprocessable_entity
